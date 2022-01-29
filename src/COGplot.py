17758dd1847f70aafd610c6066c35ca44b53f366
dkato <78598272+dkato2021@users.noreply.github.com>
2022-01-29 20:45:21 +0900

    Update COGplot.py

--- src/COGplot.py
+++ src/COGplot.py
@@ -600,8 +600,8 @@ def CLR_PCA(df = None, size = None, delta = None):#各行にCOG。
             ax2.arrow(x=0,y=0, dx=x, dy=y,
                      width=.0001, length_includes_head=True,color='m')
         ax2.scatter(pca.components_[0],  pca.components_[1], alpha=0, color='m')
-        ax1.set_title(f"Loading factor of PC1", fontsize=20/size*2)
-        ax2.set_ylabel(f"Loading factor of PC2", fontsize=20/size*2)
+        ax1.set_title(f"PC1 Loading", fontsize=20/size*2)
+        ax2.set_ylabel(f"PC2 Loading", fontsize=20/size*2)
         fig.savefig(f"./out_{get_args().evalue}/PCA/PCA_{delta}/PCA_COG_withLoadingFactor.pdf")
 
     plot_PCA(df_pca, pca, df)
@@ -622,8 +622,8 @@ def CLR_PCA(df = None, size = None, delta = None):#各行にCOG。
             ax2.arrow(x=0,y=0, dx=x, dy=y,
                      width=.0001, length_includes_head=True,color='m')
         ax2.scatter(pca.components_[0],  pca.components_[1], alpha=0, color='m')
-        ax1.set_title(f"Loading factor of PC1", fontsize=20/size*2)
-        ax2.set_ylabel(f"Loading factor of PC2", fontsize=20/size*2)
+        ax1.set_title(f"PC1 Loading", fontsize=20/size*2)
+        ax2.set_ylabel(f"PC2 Loading", fontsize=20/size*2)
         fig.savefig(f"./out_{get_args().evalue}/PCA/PCA_{delta}/PCA_COG_NoName.pdf")
 
     plot_PCA_NoName(df_pca, pca, df)
